dd3509b2b6eed0bb402a5474f730ba918434a850
EtienneMangino <etiennemangino@gmail.com>
2014-10-07 16:54:04 +0200

    version finale

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -8,17 +8,17 @@ def coach_answer(your_message)
     "I don't care, get dressed and go to work!"
   end
 
-
 end
 
 def coach_answer_enhanced(your_message)
+  prefix = (your_message.upcase == your_message) ? "I can feel your motivation! " : ""
 
-   if your_message.end_with?("?")
-      return "#{"I can feel your motivation! " if your_message.upcase == your_message}Silly question, get dressed and go to work!"
+  if your_message.end_with?("?")
+      return "#{prefix}Silly question, get dressed and go to work!"
   elsif your_message.upcase == "I AM GOING TO WORK RIGHT NOW!"
     return ""
   else
-      return "#{"I can feel your motivation! " if your_message.upcase == your_message}I don't care, get dressed and go to work!"
+    return "#{prefix}I don't care, get dressed and go to work!"
   end
 
 
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,9 +1,9 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
 puts "Speak to your coach !"
-phrase = gets.chomp
+sentence = gets.chomp
 
-while coach_answer_enhanced(phrase) != ""
-  puts coach_answer_enhanced(phrase)
-  phrase = gets.chomp
+while coach_answer_enhanced(sentence) != ""
+  puts coach_answer_enhanced(sentence)
+  sentence = gets.chomp
 end
