c27cc71974dc8849cb76bb6f23bb6b283450dc37
EtienneMangino <etiennemangino@gmail.com>
2014-10-07 15:03:41 +0200

    fin exo 5

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,9 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+puts "Speak to your coach !"
+phrase = gets.chomp
+
+while coach_answer(phrase) != ""
+  puts coach_answer(phrase)
+  phrase = gets.chomp
+end
