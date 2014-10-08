83808cfe0e122996b39746964f833ed77a1c5323
EtienneMangino <etiennemangino@gmail.com>
2014-10-08 17:21:46 +0200

    exo 7

--- 01-Ruby/02-Arrays-Enumerable/Optional-03-Beer-song/lib/beer_song.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-03-Beer-song/lib/beer_song.rb
@@ -1,3 +1,21 @@
 def beer_song(beer_start_count)
   # TODO: sing the song
+  i = beer_start_count.to_i
+
+  if i == 1
+    puts "1 bottle of beer on the wall, 1 bottle of beer!\nTake one down, pass it around, no more bottles of beer on the wall!"
+  else
+
+    puts "#{i} bottles of beer on the wall, #{i} bottles of beer!\n"
+    i -= 1
+    while i > 1
+      puts "Take one down, pass it around, #{i} bottles of beer on the wall!\n#{i} bottles of beer on the wall, #{i} bottles of beer!"
+      i -= 1
+    end
+    puts "Take one down, pass it around, 1 bottles of beer on the wall!\n1 bottle of beer on the wall, 1 bottle of beer!\nTake one down, pass it around, no more bottles of beer on the wall!"
+  end
 end
+
+
+
+beer_song(ARGV[0])
