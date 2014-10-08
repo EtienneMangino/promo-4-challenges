e6afb6b14924444670f67880feb5027e400905b0
EtienneMangino <etiennemangino@gmail.com>
2014-10-08 16:43:57 +0200

    Exo 7

--- 01-Ruby/02-Arrays-Enumerable/Optional-02-Shuffle/lib/shuffle.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-02-Shuffle/lib/shuffle.rb
@@ -1,3 +1,10 @@
 def shuffle(array)
   # TODO: shuffle the array manually
+  # 100.times do
+  #   a = array[0]
+  #   b = rand(array.size)
+  #   array[0]= array[b]
+  #   array[b]= a
+  # end
+  array.sort_by { rand }
 end
