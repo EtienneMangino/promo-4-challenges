9c1765dc3b29e66128b219a6c00a316bfc0942b8
EtienneMangino <etiennemangino@gmail.com>
2014-10-08 16:19:10 +0200

    roman un peu plys DRY ..

--- 01-Ruby/02-Arrays-Enumerable/Optional-01-Roman-numerals/lib/roman_numerals.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-01-Roman-numerals/lib/roman_numerals.rb
@@ -8,51 +8,50 @@ def old_roman_numeral(an_integer)
   text += "L" * (an_integer % 100 / 50)
   text += "X" * (an_integer % 50 / 10)
   text += "V" * (an_integer % 10 / 5)
-  text += "I" * (an_integer % 5 / 1)
-text
+  text + "I" * (an_integer % 5 / 1)
+end
+
+
+def mal_a_la_tete(an_integer, a, tableau)
+  dizieme = a / 10
+  moitie = a / 2
+  text = ""
+
+  if (an_integer % a / dizieme) == 9
+    text += tableau [a / 10 * 9]
+  elsif (an_integer % a / dizieme) >= 5
+    text += tableau[moitie]
+    text += tableau[dizieme] * (an_integer % moitie / dizieme)
+  elsif  (an_integer % a / dizieme == 4)
+    text += tableau[a / 10 * 4]
+  else
+    text += tableau [dizieme] * (an_integer % moitie / dizieme)
+  end
+  text
 end
 
 def new_roman_numeral(an_integer)
   # TODO: translate integer in text number - modern-style way
+  tableau = (0..1000).to_a
+  tableau[1] = "I"
+  tableau[4] = "IV"
+  tableau[5] =  "V"
+  tableau[9] =  "IX"
+  tableau[10] = "X"
+  tableau[40] = "XL"
+  tableau[50] =  "L"
+  tableau[90] =  "XC"
+  tableau[100] = "C"
+  tableau[400] = "CD"
+  tableau[500] =  "D"
+  tableau[900] =  "CM"
+  tableau[1000] =  "M"
+
   text = ""
   text += "M" * (an_integer / 1000)
-
-
-  if (an_integer % 1000 / 100) == 9
-    text += "CM"
-  elsif (an_integer % 1000 / 100) >= 5
-    text += "D"
-    text += "C" * (an_integer % 500 / 100)
-  elsif  (an_integer % 1000 / 100 == 4)
-    text += "CD"
-  else
-    text += "C" * (an_integer % 500 / 100)
-  end
-
-
-  if (an_integer % 100 / 10) == 9
-    text += "XC"
-  elsif (an_integer % 100 / 10) >= 5
-    text += "L"
-    text += "X" * (an_integer % 50 / 10)
-  elsif  an_integer % 100 / 10 == 4
-    text += "XL"
-  else
-    text += "X" * (an_integer % 50 / 10)
-  end
-
-  if (an_integer % 10 / 1) == 9
-    text += "IX"
-  elsif (an_integer % 10 / 1) >= 5
-    text += "V"
-    text += "I" * (an_integer % 5 / 1)
-  elsif  (an_integer % 10 / 1) == 4
-    text += "IV"
-  else
-    text += "I" * (an_integer % 5 / 1)
-  end
-
-text
+  text += mal_a_la_tete(an_integer, 1000, tableau)
+  text += mal_a_la_tete(an_integer, 100, tableau)
+  text + mal_a_la_tete(an_integer, 10, tableau)
 end
 
 puts new_roman_numeral(1454)
