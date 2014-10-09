82c704bc3cca2f082739cf4d60b08df42459d2a3
EtienneMangino <etiennemangino@gmail.com>
2014-10-09 16:17:31 +0200

    Fin 7

--- 01-Ruby/03-Blocks/Optional-03-Storing-blocks-in-Proc/lib/love_message.rb
+++ 01-Ruby/03-Blocks/Optional-03-Storing-blocks-in-Proc/lib/love_message.rb
@@ -1,18 +1,23 @@
 def tell(who, &message_blk)
   # TODO: return message addressed to who
+  "#{who}, " + message_blk.call + " !"
 end
 
 def tell_mum(&message_blk)
   # TODO: return message addressed to your mum
   # => Should call #tell of course
+  tell("mum", &message_blk)
 end
 
 def tell_with_proc(who, message_blk)
   # TODO: return message addressed to who
   # => Message is passed as Proc, not as block
+  "#{who}, " + message_blk.call + " !"
 end
 
 def tell_mum_with_proc(message_blk)
   # TODO: return message addressed to your mum
   # => Should call #tell_with_proc of course
+  tell("mum", &message_blk)
 end
+
