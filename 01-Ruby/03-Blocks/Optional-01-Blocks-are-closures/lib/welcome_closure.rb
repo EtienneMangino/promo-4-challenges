abe4eac2fc689e9279d6292e5603b7d83d0a4e40
EtienneMangino <etiennemangino@gmail.com>
2014-10-09 15:00:42 +0200

    exo 6

--- 01-Ruby/03-Blocks/Optional-01-Blocks-are-closures/lib/welcome_closure.rb
+++ 01-Ruby/03-Blocks/Optional-01-Blocks-are-closures/lib/welcome_closure.rb
@@ -2,9 +2,19 @@ count = 0
 
 welcome_block = Proc.new do |name|
   # TODO: your code goes here !
+  count += 1
+  if count == 1
+    "Welcome #{name}, you are the first here!"
+  elsif count == 2
+    "Welcome #{name}, join your 1 friend"
+  else
+    "Welcome #{name}, join your #{count - 1} friends"
+  end
+
 end
 
 puts welcome_block.call("felix")
 puts welcome_block.call("estelle")
 puts welcome_block.call("cedric")
 puts welcome_block.call("fred")
+
