aca7d806b68031d0cdc829cbc2dbcae81a2db2af
EtienneMangino <etiennemangino@gmail.com>
2014-10-09 15:39:59 +0200

    exo 7

--- 01-Ruby/03-Blocks/Optional-02-Playing-with-strings/lib/palindrome.rb
+++ 01-Ruby/03-Blocks/Optional-02-Playing-with-strings/lib/palindrome.rb
@@ -1,3 +1,24 @@
 def palindrome?(a_string)
   # TODO: check if a_string is a palindrome
+  tab_reject = %w(! , ; . : ! " ' ( ) - )
+
+  if a_string == ""
+    false
+  else
+    tab = a_string.upcase.split("")
+    tab.reject! { |x| tab_reject.include?(x) || x == " " }
+    print tab
+    if tab.size.odd?
+      a = tab[0..(tab.size / 2)]
+      print a
+      b = tab.reverse[0..(tab.size / 2)]
+      print b
+      a == b
+    else
+      a = tab[0..(tab.size / 2)]
+      b = tab.reverse[0..(tab.size / 2)]
+      a == b
+    end
+  end
 end
+puts palindrome?("wagon")
