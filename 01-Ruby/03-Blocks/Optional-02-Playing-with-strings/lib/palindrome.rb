e4ae82925e72b64a0091fb483b76a9ec97173fd8
EtienneMangino <etiennemangino@gmail.com>
2014-10-09 15:57:45 +0200

    Plus simple

--- 01-Ruby/03-Blocks/Optional-02-Playing-with-strings/lib/palindrome.rb
+++ 01-Ruby/03-Blocks/Optional-02-Playing-with-strings/lib/palindrome.rb
@@ -7,18 +7,7 @@ def palindrome?(a_string)
   else
     tab = a_string.upcase.split("")
     tab.reject! { |x| tab_reject.include?(x) || x == " " }
-    print tab
-    if tab.size.odd?
-      a = tab[0..(tab.size / 2)]
-      print a
-      b = tab.reverse[0..(tab.size / 2)]
-      print b
-      a == b
-    else
-      a = tab[0..(tab.size / 2)]
-      b = tab.reverse[0..(tab.size / 2)]
-      a == b
-    end
+    tab == tab.reverse
   end
 end
 puts palindrome?("wagon")
