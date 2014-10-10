f1b341561f5cf3900b50c8953de652be4c4cefe2
EtienneMangino <etiennemangino@gmail.com>
2014-10-10 15:43:37 +0200

    1er exo plus DRY

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
@@ -1,7 +1,4 @@
-
-def poor_calories_counter(burger, side, beverage)
-  #TODO: return number of calories for this mcDonald order
-calories = {"Cheese Burger" => 290,
+CALORIES = {"Cheese Burger" => 290,
 "Big Mac" => 300,
 "Mc Bacon" => 400,
 "Royal Cheese" => 130,
@@ -10,34 +7,30 @@ calories = {"Cheese Burger" => 290,
 "Coca" => 160,
 "Sprite" => 170}
 
-compte = calories[burger] + calories[side] + calories[beverage]
+MEAL = {
+  "Happy Meal" => ["Cheese Burger", "French fries", "Coca"],
+  "Best Of Big Mac" => ["Big Mac", "French fries", "Coca"],
+  "Best Of Royal Cheese" => ["Royal Cheese", "Potatoes", "Sprite"]
+}
+
+def poor_calories_counter(burger, side, beverage)
+  #TODO: return number of CALORIES for this mcDonald order
+
+compte = CALORIES[burger] + CALORIES[side] + CALORIES[beverage]
 
 end
 
 
 def calories_counter(*orders)
-  #TODO: return number of calories for a less constrained order
-calories = {"Cheese Burger" => 290,
-"Big Mac" => 300,
-"Mc Bacon" => 400,
-"Royal Cheese" => 130,
-"French fries" => 130,
-"Potatoes" => 130,
-"Coca" => 160,
-"Sprite" => 170}
+  #TODO: return number of CALORIES for a less constrained order
 
-meal = {
-  "Happy Meal" => ["Cheese Burger", "French fries", "Coca"],
-  "Best Of Big Mac" => ["Big Mac", "French fries", "Coca"],
-  "Best Of Royal Cheese" => ["Royal Cheese", "Potatoes", "Sprite"]
-}
 sum = 0
 
 orders.each do |x|
-  if  x == "Happy Meal" ||x == "Best Of Big Mac" || x =="Best Of Royal Cheese"
-    sum += poor_calories_counter(meal[x][0], meal[x][1], meal[x][2])
+  if  MEAL.include?(x)
+    sum += poor_calories_counter(MEAL[x][0], MEAL[x][1], MEAL[x][2])
   else
-    sum += calories[x]
+    sum += CALORIES[x]
   end
 end
 sum
