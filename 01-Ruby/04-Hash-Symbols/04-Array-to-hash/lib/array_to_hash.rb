5b54c0d53f199c11e3a9558bf81be3ced822945f
EtienneMangino <etiennemangino@gmail.com>
2014-10-10 12:18:36 +0200

    4 eme exo

--- 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
@@ -1,3 +1,12 @@
 def array_to_hash(array)
   #TODO: implement the method :)
+hash = {}
+alphabet = ("a".."z").to_a
+
+array.each_with_index { |x,i| hash[block_given? ? yield(i) : i]=x }
+hash
 end
+
+
+
+p array_to_hash(["a","b","c"])
