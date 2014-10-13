6341749153dfde2043a5820afcd65667f6b10ced
EtienneMangino <etiennemangino@gmail.com>
2014-10-13 11:02:07 +0200

    exo1

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,14 +1,13 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
-  test = phone_number.to_s.gsub(/[\s-]/,"")
-  if test =~ /(^0[1-9]\d{8}$)/
+  test = phone_number.to_s.gsub(/[\s-]/, "")
+  if test =~ /(^[0][1-9]\d{8}$)/
     true
   elsif test =~ /(^\+?33[1-9]\d{8}$)/
     true
   else
     false
   end
-
 end
 
 p french_phone_number?("062254 -87411")
--- 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
+++ 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
@@ -2,4 +2,10 @@
 
 def mail_joke(email)
   # TODO: Return (not print!) a joke suited to the email provided
+  raise ArgumentError if email.is_a?(String) == false
+
+
+
 end
+
+mail_joke(23)
