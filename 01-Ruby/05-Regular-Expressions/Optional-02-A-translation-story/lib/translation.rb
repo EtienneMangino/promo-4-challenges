3b451cffde8575c9bfdc4132d4427c377d8db6ab
EtienneMangino <etiennemangino@gmail.com>
2014-10-13 15:22:09 +0200

    fin

--- 01-Ruby/05-Regular-Expressions/Optional-02-A-translation-story/lib/translation.rb
+++ 01-Ruby/05-Regular-Expressions/Optional-02-A-translation-story/lib/translation.rb
@@ -19,24 +19,20 @@ STRINGS = {
   }
 }
 
-def translation(a_string, a_language="en")
+def translation(a_string, a_language = "en")
   # TODO: your code goes here
   a_language = a_language.to_sym
   english = :en
 
-  chemin = a_string.scan(/\w+/).map! { |x| x.to_sym}
+  route = STRINGS
+  chemin = a_string.scan(/\w+/).map!(&:to_sym)
 
-  if a_string == "unvalid.path"
-    ""
-  else
-    if chemin.size == 2
-       STRINGS[chemin[0]][chemin[1]][STRINGS[chemin[0]][chemin[1]].include?(a_language) ? a_language : english]
-    elsif chemin.size == 3
-       STRINGS[chemin[0]][chemin[1]][chemin[2]][STRINGS[chemin[0]][chemin[1]].include?(a_language) ? a_language : english]
-    else
-      ''
-    end
+  chemin.each do |x|
+    route = route[x]
+    return "" if route.nil?
   end
+
+  route[route.include?(a_language) ? a_language : english]
 end
 
 translation('home.intro', 'fr')
