1e4f3609526b1170f55804a892074b2bb313454b
EtienneMangino <etiennemangino@gmail.com>
2014-10-13 14:17:38 +0200

    exo 5

--- 01-Ruby/05-Regular-Expressions/Optional-01-Provider-grouping/lib/provider_grouping.rb
+++ 01-Ruby/05-Regular-Expressions/Optional-01-Provider-grouping/lib/provider_grouping.rb
@@ -1,7 +1,21 @@
 def group_mails(emails)
   # TODO: group email by provider
+  hashmail = {}
+
+  emails.each do |x|
+    next if x.match(/^.*@\w*\..*$/).nil?
+    if hashmail.include?(x.match(/^.*@(\w*)\..*$/)[1])
+      hashmail[x.match(/^.*@(\w*)\..*$/)[1]] << x
+    else
+      hashmail[x.match(/^.*@(\w*)\..*$/)[1]] = [x]
+    end
+  end
+
+  hashmail
 end
 
 def provider?(email, provider)
   # TODO: return true if email is of given provider
+  email.match(/^.*@(\w*)\..*$/)[1] == provider
 end
+
