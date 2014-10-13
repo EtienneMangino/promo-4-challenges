60561ab7ed16248e80852ce263a9c42c7e898adc
EtienneMangino <etiennemangino@gmail.com>
2014-10-13 11:39:21 +0200

    exo3

--- 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
+++ 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
@@ -1,11 +1,33 @@
 # Encoding: utf-8
 
+JOKE = {
+  "hotmail.fr" => "Génération msn",
+  "lewagon.org" => "you're skilled and capable",
+  "gmail.com" => "you're an average but modern person",
+  "live.com" => "aren't you born before 1973?"
+}
+
+JOKE_DEBUT = {
+  "hotmail.fr" => "Hey ",
+  "lewagon.org" => "Well done ",
+  "gmail.com" => "",
+  "live.com" => ""
+}
+
 def mail_joke(email)
   # TODO: Return (not print!) a joke suited to the email provided
-  raise ArgumentError if email.is_a?(String) == false
+  if email.is_a?(String) == false || email.match(/^(.*)@\w*\..*$/).nil?
+    fail ArgumentError
+  end
 
+  nom = email.match(/^(.*)@/)
+  domaine = email.match(/@(\w*\..*)$/)
 
-
+  if JOKE.include?(domaine[1])
+    "#{JOKE_DEBUT[domaine[1]]}#{nom[1].gsub(/\./, " ")}, #{JOKE[domaine[1]]}"
+  else
+    "Sorry #{nom[1].gsub(/\./, " ")}, we don't know how to judge '#{domaine[1]}'"
+  end
 end
 
-mail_joke(23)
+p mail_joke("etienne@hotmail.com")
