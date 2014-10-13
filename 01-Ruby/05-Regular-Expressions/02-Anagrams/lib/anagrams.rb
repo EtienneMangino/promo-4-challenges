b06dcb774da555e8e443cc15e98cc6913a028eea
EtienneMangino <etiennemangino@gmail.com>
2014-10-13 10:39:54 +0200

    exo1

--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
@@ -1,8 +1,16 @@
 def anagrams?(a_string, another_string)
   #TODO: implement the obvious method to test if two words are anagrams
+  a_string.gsub!(/[\W_]/,'')
+  another_string.gsub!(/[\W_]/,'')
+  a_string.downcase.chars.sort.join == another_string.downcase.chars.sort.join
 end
 
 def anagrams_on_steroids?(a_string, another_string)
   #TODO: implement the improved method
+  hash = {
+    text1: a_string.gsub(/[\W_]/,''),
+    text2: another_string.gsub(/[\W_]/,'')
+  }
+  hash[:text1].downcase.chars.sort.join == hash[:text2].downcase.chars.sort.join
 end
-  
+
