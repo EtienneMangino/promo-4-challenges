387137414554f77d492ff2d59827f2b76429f510
EtienneMangino <etiennemangino@gmail.com>
2014-10-27 12:15:58 +0100

    exo2

--- 03-AR-Database/03-ActiveRecord-Basics/02-Models/app/controllers/posts_controller.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/02-Models/app/controllers/posts_controller.rb
@@ -9,25 +9,46 @@ class PostsController
     # DO NOT WRITE SQL QUERIES
     # TODO: gather all posts from the database
     # TODO: give them to the view to be printed
+    posts = Post.all
+    @view.list_all_posts(posts)
   end
 
   def create
     # DO NOT WRITE SQL QUERIES
     # TODO: create a post
+    name = @view.ask_for_name
+    url = @view.ask_for_url
+    new_post = Post.new(name: name, url: url)
+    new_post.save
   end
 
   def update
     # DO NOT WRITE SQL QUERIES
     # TODO: update a post
+    index
+    id = @view.ask_for_id
+    post_to_update = Post.find(id)
+    post_to_update.name = @view.ask_for_name
+    post_to_update.url = @view.ask_for_url
+    post_to_update.save
   end
 
   def destroy
     # DO NOT WRITE SQL QUERIES
     # TODO: destroy a post
+    index
+    id = @view.ask_for_id
+    post_to_delete = Post.find(id)
+    post_to_delete.destroy
   end
 
   def upvote
     # DO NOT WRITE SQL QUERIES
     # TODO: increment the `votes` column for a post
+    index
+    id = @view.ask_for_id
+    post_to_upvote = Post.find(id)
+    post_to_upvote.votes += 1
+    post_to_upvote.save
   end
 end
--- 03-AR-Database/03-ActiveRecord-Basics/02-Models/app/views/post_view.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/02-Models/app/views/post_view.rb
@@ -1,3 +1,25 @@
 class PostView
   # TODO: implement some methods here when controller needs to `puts` or `gets`
+  def list_all_posts(posts)
+    posts.each do |post|
+      puts "id : #{post.id} name : #{post.name}, url : #{post.url}, votes : #{post.votes}"
+    end
+  end
+
+  def ask_for_name
+    puts "What name ?"
+    gets.chomp
+  end
+
+
+  def ask_for_url
+    puts "What url ?"
+    gets.chomp
+  end
+
+  def ask_for_id
+    puts "What id ?"
+    Integer(gets.chomp)
+  end
+
 end
