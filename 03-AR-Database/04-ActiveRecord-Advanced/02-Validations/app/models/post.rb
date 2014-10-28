5052732f950eea485b2984bf0ed15ab5c92783b3
EtienneMangino <etiennemangino@gmail.com>
2014-10-28 12:10:11 +0100

    tt

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   # TODO: Add some validation
+  validates_presence_of :name, :url, :user
+  validates :name, length: { minimum: 5 }
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :url, format: { with: /\A(?:http:\/\/)?(?:([\w-]+)\.)?([\w-]+)\.([\w]+)\/?(?:([^?#$]+))?(?:\?([^#$]+))?(?:#(.*))?\z/, message: "invalid url" }
 end
--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -1,5 +1,33 @@
 class User < ActiveRecord::Base
   has_many :posts
 
-  # TODO: Add some validation
+  # TODO: Add some validation*
+  before_validation :change_email
+
+  validates :username, :email, presence: true
+  validates :username, uniqueness: true
+
+  validates :email, format: { with: /\A\w+@\w+\.\w{2,3}\z/ , message: 'invalid email' }
+
+
+  private
+
+  def change_email
+    self.email = self.email.gsub(/\s/, "") unless email.nil?
+    # self.email = email.lstrip.rstrip unless email.nil?
+  end
+
+
 end
+
+# class User < ActiveRecord::Base
+#  has_many :posts
+#  before_validation :stripspaces
+#  validates :username, :email, presence: true
+#  validates :email, format: { with: /\A\w+@\w+\.\w{2,3}\z/, message: "invalid email" }
+#  validates :username, uniqueness: true
+# end
+
+# def stripspaces
+#  self.email = email.lstrip.rstrip unless email.nil?
+# end
