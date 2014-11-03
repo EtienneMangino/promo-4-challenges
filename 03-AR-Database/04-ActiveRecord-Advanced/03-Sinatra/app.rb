5affe48a29515a843422ff8e66b80197f547669c
EtienneMangino <etiennemangino@gmail.com>
2014-11-03 12:30:04 +0100

    exo1

--- 03-AR-Database/04-ActiveRecord-Advanced/03-Sinatra/app.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/03-Sinatra/app.rb
@@ -11,7 +11,31 @@ set :views, proc { File.join(root, "app/views") }
 get '/' do
   # TODO: fetch posts from database.
   #       to pass them to the view, store them in an instance variable
-
+  @posts = Post.all
 
   erb :posts  # The rendered HTML is in app/views/posts.erb
+end
+
+post '/' do
+  @post = Post.new(name: params[:name], url: params[:url])
+  if @post.valid?
+    @post.save
+    redirect "/"
+  else
+    erb  :post
+    # redirect "/"
+  end
+end
+
+post '/vote' do
+  post = Post.find(params[:id])
+  post.votes += 1
+  post.save
+  redirect "/"
+end
+
+get '/delete' do
+  post = Post.find(params[:id])
+  post.destroy
+  redirect "/"
 end
--- 03-AR-Database/04-ActiveRecord-Advanced/03-Sinatra/app/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/03-Sinatra/app/models/post.rb
@@ -1,5 +1,10 @@
 class Post < ActiveRecord::Base
-  belongs_to :user
+
+  validates_presence_of :name, :url
+  validates :name, length: { minimum: 5 }
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :url, format: { with: /\A(?:http:\/\/)?(?:([\w-]+)\.)?([\w-]+)\.([\w]+)\/?(?:([^?#$]+))?(?:\?([^#$]+))?(?:#(.*))?\z/, message: "invalid url" }
 
   # TODO: Add some validation
+  default_scope { order('votes DESC') }
 end
